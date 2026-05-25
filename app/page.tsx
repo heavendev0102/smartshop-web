b15d64da32b2a74d1ca1b6f2ac931ccfa662b3ab
Dhruvi Pattiwala <pattiwaladhruvi@gmail.com>
2026-05-25 16:41:51 +0530

    remove sections from home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@
 import { getStorefront } from "@/app/services/getStorefront";
 import Image from "next/image";
 // import phoneImage from "../public/Home/image.png";
-import SSD from "../public/Home/ssd.png";
-import Apro from "../public/Home/Apro.png";
-import phone_of_section2 from "../public/Home/phone_of_section2.png";
-import headphone from "../public/Home/headphone.png";
+// import SSD from "../public/Home/ssd.png";
+// import Apro from "../public/Home/Apro.png";
+// import phone_of_section2 from "../public/Home/phone_of_section2.png";
+// import headphone from "../public/Home/headphone.png";
 import { ProductType, StorefrontResponse, Product } from "@/app/util/type";
 import ProductCard from "./(user)/_components/ProductCard";
 // import productBanner from "../public/Home/banner.png";
 import FeaturedProducts from "./(user)/_components/FeatureProductCard";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import SummerProducts from "./(user)/_components/SummerProducts";
+// import SummerProducts from "./(user)/_components/SummerProducts";
 import HeroCarousel from "./(user)/_components/HeroCarousel";
 const Page = () => {
 
@@ -34,7 +34,7 @@ const Page = () => {
             label: storeData?.featured?.name || "Featured",
         },
     ];
-    const featuredSummerProducts =  storeData?.featured?.products || [];
+    // const featuredSummerProducts =  storeData?.featured?.products || [];
     
     const allProducts: Product[] = [
         ...(storeData?.new_arrivals?.products || []),
@@ -94,31 +94,31 @@ const Page = () => {
     ).slice(0, 4);
     const featuredProducts =  storeData?.featured?.products?.slice(0, 4) || [];
    
-    const ps5 = filteredProducts.find((p) =>
-        p.categories.some((c) => c.slug === "gaming")
-    );
+    // const ps5 = filteredProducts.find((p) =>
+    //     p.categories.some((c) => c.slug === "gaming")
+    // );
 
-    const vision = filteredProducts.find((p) =>
-        p.categories.some((c) => c.slug === "cameras")
-    );
+    // const vision = filteredProducts.find((p) =>
+    //     p.categories.some((c) => c.slug === "cameras")
+    // );
 
-    const airpods = filteredProducts.find((p) =>
-        p.categories.some((c) => c.slug === "headphones")
-    );
+    // const airpods = filteredProducts.find((p) =>
+    //     p.categories.some((c) => c.slug === "headphones")
+    // );
 
-    const macbook = filteredProducts.find((p) =>
-        p.categories.some((c) => c.slug === "smartwatches")
-    );
+    // const macbook = filteredProducts.find((p) =>
+    //     p.categories.some((c) => c.slug === "smartwatches")
+    // );
 
     return (
         <>
             <HeroCarousel slides={heroSlides} />
-            <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-stretch">
+            {/* <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-stretch">
 
-                {/* LEFT SECTION */}
+               
                 <div className="flex flex-col gap-6 h-full">
 
-                    {/* TOP BIG CARD */}
+                  
                     <div
                         className="flex flex-col sm:flex-row items-center bg-white p-6 gap-6 h-full cursor-pointer"
                         onClick={() => router.push(`/ProductDetail/${ps5?.id}`)}
@@ -141,10 +141,9 @@ const Page = () => {
                         </div>
                     </div>
 
-                    {/* BOTTOM TWO CARDS */}
+                 
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 auto-rows-fr">
 
-                        {/* AIRPODS CARD */}
                         <div
                             className="flex items-center bg-gray-200 p-6 gap-4 h-full cursor-pointer"
                             onClick={() => router.push(`/ProductDetail/${airpods?.id}`)}
@@ -170,7 +169,7 @@ const Page = () => {
                             </div>
                         </div>
 
-                        {/* VISION CARD */}
+                    
                         <div
                             className="flex items-center bg-[#353535] text-white p-6 gap-4 h-full cursor-pointer"
                             onClick={() => router.push(`/ProductDetail/${vision?.id}`)}
@@ -196,7 +195,7 @@ const Page = () => {
                     </div>
                 </div>
 
-                {/* RIGHT SECTION (IPAD) */}
+            
                 <div
                     className="flex flex-col md:flex-row items-center justify-between bg-gray-200 p-6 md:p-10 gap-6 h-full cursor-pointer"
                     onClick={() => router.push(`/ProductDetail/${macbook?.id}`)}
@@ -220,7 +219,7 @@ const Page = () => {
                     />
                 </div>
 
-            </section>
+            </section> */}
             <section className="bg-gray-100 py-8 px-4 md:px-10 lg:px-20">
                 <div className="flex items-center justify-between mb-6">
                     <h2 className="text-xl md:text-2xl font-bold text-gray-800">
@@ -316,7 +315,7 @@ const Page = () => {
                     </div>
                 </div>
             </section>
-            <SummerProducts featuredProducts={featuredSummerProducts}/>
+            {/* <SummerProducts featuredProducts={featuredSummerProducts}/> */}
             {/* <Image src={productBanner} alt="banner" className="w-full h-auto mt-5 object-cover" /> */}
         </>
     )
